3b763779b28dedc769f613d7198a3e1d91ae8ca4
Omar Contreras <omararturo15@hotmail.com>
2016-10-31 17:53:32 -0500

    correcion de errores plan de accion, tablas dinamicas

--- app/controller/planAccion.php
+++ app/controller/planAccion.php
@@ -15,8 +15,14 @@ class PlanAccion extends Controller{
     
     public function agregarPlanAccion(){
         $contenido=$this->getTemplate("./app/views/PlanAccion/planAccion.html");
+        $tablasProblemas=$this->getTemplate("./app/views/PlanAccion/componentes/tablas-problemas.html");
         $this->view = $this->renderView($this->view, "{{TITULO}}","Agregar Plan De Acción");
         $this->view = $this->renderView($this->view, "{{SESION}}", $this->menu);
+        //PRUEBAS
+       $contenido=$this->renderView($contenido, "{{CANT_PROBLEMAS}}",1);
+       $tablasProblemas=$this->renderView($tablasProblemas, "{{NUM_PROBLEMA}}",1);
+       $contenido=$this->renderView($contenido, "{{TABLAS_PROBLEMAS}}",$tablasProblemas);
+        //
         $this->view = $this->renderView($this->view, "{{CONTENT}}", $contenido);
         $this->view = $this->renderView($this->view, "{{ASESOR}}", $this->planAccionModel->consultarNombreAsesor());
         $this->showView($this->view);
