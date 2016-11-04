edd18677395daad8bec47d9e6a501eb6557747bd
Omar Contreras <Omar Contreras>
2016-11-03 19:42:44 -0500

    listas ordenas, aspectos a mejorar, diag idea

--- app/controller/diagnosticoIdea.php
+++ app/controller/diagnosticoIdea.php
@@ -142,7 +142,7 @@
             $dificultad = $this->diagnosticoIdeaModel->consultarDificultad($row['Num_consecutivo']);
             $aspectos = "";
             for($i = 0; $i < sizeof($dificultad); $i++){
-                $aspectos.= $dificultad[$i]['numero']."-".$dificultad[$i]['descripcion']."<br>";
+                $aspectos.= "<li>".$dificultad[$i]['descripcion']."</li>";
             }
             $this->view = $this->renderView($this->view, "{{Aspectos}}", $aspectos);
             $this->showView($this->view);  
