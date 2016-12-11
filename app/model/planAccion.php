12f6a3d942382a0f4547086c1c8b17e514498f4a
elizarv <eramirezvillamizar@gmail.com>
2016-12-11 12:06:03 -0500

    problemas diagnostico empresa parte 1 eliza es lo maximo

--- app/model/planAccion.php
+++ app/model/planAccion.php
@@ -73,6 +73,7 @@ class PlanAccionModel extends Model {
         return $row;
     }
 
+    //consulta los posibles problemas que se van a listar para el plan de accion de la idea seleccionada
     public function consultarProblemasDiagIdea($numConsecutivo){
         $this->connect();
         $array=array();
@@ -85,8 +86,28 @@ class PlanAccionModel extends Model {
         return $array;
     }
 
+
+    //consulta los posibles problemas que se van a listar para el plan de accion de la empresa seleccionada
     public function consultarProblemasDiagEmpresa($numConsecutivo){
+        $this->connect();
+        $array=array();
+        $consulta1 = "SELECT mision, vision, objetivos, estrategias, plan_accion, participacion_empleados, empleados_conocen, estrategias_crecimiento, organigrama, procesos_documentados, procesos_evaluacion, procesos_automatizar, max_colaboradores, clima_laboral, motivacion_empleados, define_acciones, sistema_control, comparar_planeado_eje, clave_desempenio, seguimiento_indicadores, contrata_direc_personal, combina_forma_contratar, procesos_establecidos, recompensas_establecidas, empleados_necesarios, depto_mercadeo_ventas, msj_marketing_claro, plan_mercadeo, implementa_plan, cronograma_marketing, perfil_cliente, clasificacion_cliente, web_ventas, tecnologia_seguimiento, sistema_compatibilidad, contabilidad_aldia, aplica_normas_contabilidad, planificacion_financiera_formal, margen_rentabilidad, margen_rentabilidad_pos, ingresos_cumplen_objetivos, suficiente_capital, flujo_caja_positivo, costo_producto, presupuesto_estable, desiciones_analisis, programa_produccion, corresponde_demanda_mercado, produccion_planes, pontrola_calidad_producto, problemas_abastecimiento, Adquisicion_maquinaria, Planes_contingencia_maprima, Inventarios_periodicos, Seguimiento_inventarios, eficiente_dist_trabajo FROM diagnostico_empresa WHERE id_diagnostico_emp =$numConsecutivo";
+        $query1 = $this->query($consulta1);
+        $consulta2 = "SELECT id, descripcion FROM desc_problemas";
+        $query2 = $this->query($consulta2);
+        $this->terminate();
+        $row = mysqli_fetch_array($query1);
         
+        while($desc = mysqli_fetch_array($query2)){
+            if($row[$desc['id']]!='Si'){
+                array_unshift($array, $desc['descripcion']);
+            }
+            if($desc['id']=='problemas_abastecimiento' && $row[$desc['id']]=='Si'){
+                array_unshift($array, $desc['descripcion']);
+            }
+        }
+        return $array;
+
     }
     }
 ?>
